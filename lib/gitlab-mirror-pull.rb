5b22f68fa04cc7f773157e01484e58ae18e0138f
ochorocho <rothjochen@gmail.com>
2018-06-13 22:06:45 +0200

    Reformat code

--- lib/gitlab-mirror-pull.rb
+++ lib/gitlab-mirror-pull.rb
@@ -4,78 +4,78 @@ require 'logger'
 require 'yaml'
 
 class GitlabMirrorPull
-	
-	attr_accessor :config, :log_level
-	
-	# Parse commandline arguments
-	#
-	# == Parameters:
-	# config:: 
-	#	Path to config file (e.g. ../config.example.yml)
-	#
-	# log_level::
-	#   Set log level. Possible values: `Logger::INFO`, `Logger::WARN`, `Logger::ERROR`, `Logger::DEBUG`
-	#
-	# == Returns:
-	# Returns `@log` and `@config`
-	# 
-	def initialize(config = File.join(File.dirname(__FILE__), "../config.example.yml"), log_level = Logger::ERROR)
-		
-		# Configure Logger
-		@log = Logger.new(STDOUT)
-		@log.level = log_level
-		@config = YAML.load_file(config)
-		self.fetch_repositories
-	end
 
-	# Prepare list of repositories
-	#
-	# == Returns:
-	# List of repositories to update using `git fetch`. Excludes `*.wiki` and repositories defined in `config.yml -> git -> repos`
-	# 
-	def repositories_to_fetch
-		# Find all .git Repositories - Ignore *.wiki.git
-		repos = Dir.glob("#{config['git']['repos']}/*/*{[!.wiki]}.git")
-		
-		# Build up array of NOT ignored repositories
-		delete_path = []
-		@config['ignore'].each do |ignored|
-			path = File.join(@config['git']['repos'], ignored)
-			delete_path += repos.grep /^#{path}/
-			repos.delete(delete_path)
-		end
-		
-		return repos - delete_path 
+  attr_accessor :config, :log_level
 
-	end
+  # Parse commandline arguments
+  #
+  # == Parameters:
+  # config::
+  #	Path to config file (e.g. ../config.example.yml)
+  #
+  # log_level::
+  #   Set log level. Possible values: `Logger::INFO`, `Logger::WARN`, `Logger::ERROR`, `Logger::DEBUG`
+  #
+  # == Returns:
+  # Returns `@log` and `@config`
+  #
+  def initialize(config = File.join(File.dirname(__FILE__), "../config.example.yml"), log_level = Logger::ERROR)
 
-	# Fetch repositories return by `repositories_to_fetch`
-	#
-	# == Returns:
-	# Logging infos on fetched repos
-	# 
-	def fetch_repositories
-		# Init git settings
-		Git.configure do |config|
-			config.binary_path = "#{@config['git']['path']}"
-		end		
+    # Configure Logger
+    @log = Logger.new(STDOUT)
+    @log.level = log_level
+    @config = YAML.load_file(config)
+    self.fetch_repositories
+  end
 
-		# Loop through repos and fetch it
-		repos_to_fetch = self.repositories_to_fetch
-		repos_to_fetch.each do |repo| 	
-			if File.directory?(repo)
-				# Get branches
-				g = Git.bare("#{repo}", :log => @log)
-				g.remotes.each do |remote|
-					# Determine which "remote" to fetch e.g. "git fetch github"
-					if @config['provider'].include?("#{remote}")
-						@log.info("Fetching remote #{remote} in #{repo}")
-						g.remote(remote).fetch
-					end
-				end
-			end
-		end		
+  # Prepare list of repositories
+  #
+  # == Returns:
+  # List of repositories to update using `git fetch`. Excludes `*.wiki` and repositories defined in `config.yml -> git -> repos`
+  #
+  def repositories_to_fetch
+    # Find all .git Repositories - Ignore *.wiki.git
+    repos = Dir.glob("#{config['git']['repos']}/*/*{[!.wiki]}.git")
 
-	end
+    # Build up array of NOT ignored repositories
+    delete_path = []
+    @config['ignore'].each do |ignored|
+      path = File.join(@config['git']['repos'], ignored)
+      delete_path += repos.grep /^#{path}/
+      repos.delete(delete_path)
+    end
+
+    return repos - delete_path
+
+  end
+
+  # Fetch repositories return by `repositories_to_fetch`
+  #
+  # == Returns:
+  # Logging infos on fetched repos
+  #
+  def fetch_repositories
+    # Init git settings
+    Git.configure do |config|
+      config.binary_path = "#{@config['git']['path']}"
+    end
+
+    # Loop through repos and fetch it
+    repos_to_fetch = self.repositories_to_fetch
+    repos_to_fetch.each do |repo|
+      if File.directory?(repo)
+        # Get branches
+        g = Git.bare("#{repo}", :log => @log)
+        g.remotes.each do |remote|
+          # Determine which "remote" to fetch e.g. "git fetch github"
+          if @config['provider'].include?("#{remote}")
+            @log.info("Fetching remote #{remote} in #{repo}")
+            g.remote(remote).fetch
+          end
+        end
+      end
+    end
+
+  end
 
 end
