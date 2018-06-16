4a174b133eaf6b906fda0897ddab3760b6dfc655
ochorocho <rothjochen@gmail.com>
2018-06-16 23:52:58 +0200

    refs #3 - Use apline only, add ruby 2.5 container

--- Dockerfiles/build.sh
+++ Dockerfiles/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-docker build --pull . -f Dockerfile.ruby23 -t ochorocho/gitlab-mirror-pull:ruby-2.3 && \
 docker build --pull . -f Dockerfile.alpine-ruby24 -t ochorocho/gitlab-mirror-pull:alpine-ruby-2.4 && \
-docker build --pull . -f Dockerfile.ruby24 -t ochorocho/gitlab-mirror-pull:ruby-2.4
+docker build --pull . -f Dockerfile.alpine-ruby25 -t ochorocho/gitlab-mirror-pull:alpine-ruby-2.5
