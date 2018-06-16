7c3c01ed2fbea7f71693fa5a4b1598c05b99a3a0
ochorocho <rothjochen@gmail.com>
2018-06-16 23:34:42 +0200

    refs #3 - Add alpine image

--- Dockerfiles/build.sh
+++ Dockerfiles/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker build --pull . -f Dockerfile.ruby23 -t ochorocho/gitlab-mirror-pull:ruby23 && \
-docker build --pull . -f Dockerfile.ruby24 -t ochorocho/gitlab-mirror-pull:ruby24
+docker build --pull . -f Dockerfile.ruby23 -t ochorocho/gitlab-mirror-pull:ruby-2.3 && \
+docker build --pull . -f Dockerfile.alpine-ruby24 -t ochorocho/gitlab-mirror-pull:alpine-ruby-2.4 && \
+docker build --pull . -f Dockerfile.ruby24 -t ochorocho/gitlab-mirror-pull:ruby-2.4
