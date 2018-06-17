dc010abcfb707dd052756267eac4e752a0bdaf28
ochorocho <rothjochen@gmail.com>
2018-06-17 14:53:05 +0200

    refs #4 - share variables accross methods

--- tests/gitlab_mirror_pull_test.rb
+++ tests/gitlab_mirror_pull_test.rb
@@ -15,24 +15,25 @@ class GitlabMirrorPullTest < Test::Unit::TestCase
     repos_to_create = ["repo_1","repo_2","repo_3"]
     repos_to_create.each do |repo_name|
 
+      # Set paths and names
       repo_name = repo_name
       repo_from = File.join(File.dirname(__FILE__), "../fixtures/from_repos/", repo_name)
       repo_to = File.join(File.dirname(__FILE__), "../fixtures/to_repos/")
 
-      # Create from Repo
-      from = Git.init(repo_from)
+      # Create from Repo and add content
+      @from = Git.init(repo_from)
       path = File.join(File.dirname(__FILE__), "../fixtures/from_repos/#{repo_name}/mirror.txt")
       content = "To be mirrored"
       File.open(path, "w+") do |f|
         f.write(content)
       end
-      from.add
-      from.commit('Example mirror')
+      @from.add
+      @from.commit('Example mirror')
 
       # Init clone
-      to = Git.clone(repo_from, repo_name, :path => repo_to)
+      @to = Git.clone(repo_from, repo_name, :path => repo_to)
       # Add remote to fetch
-      to.add_remote('github', repo_from)
+      @to.add_remote('github', repo_from)
     end
   end
 
