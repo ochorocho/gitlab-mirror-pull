3df22f10985b6f4f4aea0332461abdfa5e54b8ee
ochorocho <rothjochen@gmail.com>
2018-06-21 00:42:02 +0200

    refs #4 - Add test for 'fetch_repositories' method

--- tests/gitlab_mirror_pull_test.rb
+++ tests/gitlab_mirror_pull_test.rb
@@ -1,45 +1,59 @@
-require 'test/unit'
+require 'minitest/autorun'
 require 'yaml'
 require 'git'
 require 'logger'
+require 'fileutils'
 
 require_relative '../lib/gitlab_mirror_pull.rb'
 
-class GitlabMirrorPullTest < Test::Unit::TestCase
+class GitlabMirrorPullTest < Minitest::Test
 
   def setup
-    @config = File.join(File.dirname(__FILE__), "config.tests.yml")
-    @log_level = Logger::ERROR
-    @pull = GitlabMirrorPull.new(@config, @log_level)
+    config = File.join(File.dirname(__FILE__), "config.tests.yml")
+    log_level = Logger::ERROR
+    @pull = GitlabMirrorPull.new(config, log_level)
+    @config_current = YAML.load_file(File.join(File.dirname(__FILE__), "config.tests.yml"))
 
-    repos_to_create = ["repo_1","repo_2","repo_3"]
-    repos_to_create.each do |repo_name|
+    # Create repos for testing
+    @repos_to_create = ["repo_1.git", "repo_2.git", "repo_3.git"]
+    @repos_to_create.each do |repo_name|
 
       # Set paths and names
       repo_name = repo_name
-      repo_from = File.join(File.dirname(__FILE__), "../fixtures/from_repos/", repo_name)
-      repo_to = File.join(File.dirname(__FILE__), "../fixtures/to_repos/")
+      repo_from = File.expand_path File.join(File.dirname(__FILE__), "../fixtures/from_repos/#{repo_name}")
+      repo_to = File.expand_path File.join(File.dirname(__FILE__), "../fixtures/to_repos/user_group/")
 
       # Create from Repo and add content
-      @from = Git.init(repo_from)
-      path = File.join(File.dirname(__FILE__), "../fixtures/from_repos/#{repo_name}/mirror.txt")
+      from = Git.init(repo_from)
+      path = File.expand_path File.join(File.dirname(__FILE__), "../fixtures/from_repos/#{repo_name}/mirror.txt")
       content = "To be mirrored"
       File.open(path, "w+") do |f|
         f.write(content)
       end
-      @from.add
-      @from.commit('Example mirror')
+      from.add
+      from.commit('Example mirror')
 
-      # Init clone
-      @to = Git.clone(repo_from, repo_name, :path => repo_to)
+      # Init mirror
+      to = Git.clone(repo_from, repo_name, :path => repo_to, :bare => true)
+
       # Add remote to fetch
-      @to.add_remote('github', repo_from)
+      to.add_remote('github', repo_from)
     end
   end
 
   def test_fetch_repositories
-    @repo = @pull.fetch_repositories
-    puts @repo.to_yaml
-    assert_equal("/repositories/ruby/gitlab-mirror-pull.git", @repo[0], "Expexted valid repository path")
+    repos = @pull.fetch_repositories
+    @repo_static_paths = @repos_to_create.map { |n| "#{@config_current['git']['repos']}/user_group/#{n}" }
+    compared_repos = @repo_static_paths - repos
+    assert_equal(compared_repos.empty?, true, "Expexted empty array")
+    @repo_static_paths.each do |path|
+      assert_equal(File.exists?(path + '/config'), true, "Expect valid bare repository")
+    end
   end
+
+  def teardown
+    FileUtils.remove_dir(File.join(File.dirname(__FILE__), "../fixtures/from_repos"))
+    FileUtils.remove_dir(File.join(File.dirname(__FILE__), "../fixtures/to_repos"))
+  end
+
 end
